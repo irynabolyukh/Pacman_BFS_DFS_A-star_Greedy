2f38eb9e1014beb8dd4e8a80f339d199672b07d6
yar0slava <yavacollins@gmail.com>
2020-10-04 16:49:49 -0700

    DFS BFS classes

--- src/com/zetcode/PathSearcher.java
+++ src/com/zetcode/PathSearcher.java
@@ -5,43 +5,65 @@ import java.util.List;
 
 public abstract class PathSearcher {
 
-    private short[][] screenData;
+    public short[][] screenData;
 
-    List<Point> path = new ArrayList<Point>();
+    public List<Point> path = new ArrayList<Point>();
 
-    int curr_x =0, curr_y=0;
-    Point.Direction curr_dir;
+    public int curr_x =0, curr_y=0;
 
     private boolean canMoveLeft(){
+        short ch;
+        ch = screenData[curr_y][curr_x];
+        if((ch & 1) != 0){
+            return false;
+        }
         return true;
-    };
-
-    private boolean canMoveRight(){
-        return true;
-    };
-
+    }
     private boolean canMoveUp(){
-        return true;
-    };
+        short ch;
+        ch = screenData[curr_y][curr_x];
 
+        if((ch & 2) != 0){
+            return false;
+        }
+        return true;
+    }
+    private boolean canMoveRight(){
+        short ch;
+        ch = screenData[curr_y][curr_x];
+
+        if((ch & 4) != 0){
+            return false;
+        }
+        return true;
+    }
     private boolean canMoveDown(){
+        short ch;
+        ch = screenData[curr_y][curr_x];
+
+        if((ch & 8) != 0){
+            return false;
+        }
         return true;
-    };
-
-    private void moveLeft(){
-
     }
 
-    private void moveRight(){
-
+    public void moveLeft(){
+        path.add(new Point(curr_x, curr_y, Point.Direction.LEFT));
+        curr_x -=1;
+    }
+    public void moveRight(){
+        path.add(new Point(curr_x, curr_y, Point.Direction.RIGHT));
+        curr_x +=1;
+    }
+    public void moveUp(){
+        path.add(new Point(curr_x, curr_y, Point.Direction.UP));
+        curr_y -=1;
+    }
+    public void moveDown(){
+        path.add(new Point(curr_x, curr_y, Point.Direction.DOWN));
+        curr_y += 1;
     }
 
-    private void moveUp(){
-
-    }
-
-    private void moveDown(){
-
-    }
+    public abstract void searchForPath();
 
 }
