14b33b5e46b3b314e8f71a7d6a2554c331e12e98
yar0slava <yavacollins@gmail.com>
2020-10-10 20:38:12 -0700

    finished A*, removed odd code

--- src/com/zetcode/Pacman.java
+++ src/com/zetcode/Pacman.java
@@ -6,12 +6,10 @@ import javax.swing.JFrame;
 public class Pacman extends JFrame {
 
     public Pacman() {
-
         initUI();
     }
 
     private void initUI() {
-
         add(new Board());
 
         setTitle("Pacman");
@@ -21,18 +19,6 @@ public class Pacman extends JFrame {
     }
 
     public static void main(String[] args) {
-
-//        PathSearcherBFS searcherBFS = new PathSearcherBFS();
-//        searcherBFS.searchForPath();
-//        System.out.println("ALL VISITED:");
-//        for(int i=0;i<searcherBFS.path.size();i++){
-//            System.out.println(searcherBFS.path.get(i));
-//        }
-//        System.out.println("\n\nRIGHT PATH:");
-//        for(int i=0;i<searcherBFS.path_stack.size();i++){
-//            System.out.println(searcherBFS.path_stack.get(i));
-//        }
-
         EventQueue.invokeLater(() -> {
 
             Pacman ex = new Pacman();
--- src/com/zetcode/PathSearcherAStar.java
+++ src/com/zetcode/PathSearcherAStar.java
@@ -1,8 +1,6 @@
 package com.zetcode;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 
 public class PathSearcherAStar extends PathSearcher {
 
@@ -40,54 +38,58 @@ public class PathSearcherAStar extends PathSearcher {
 
         crt = new MyPoint(0, 0, MyPoint.Direction.RIGHT);
 
-        // add initial node to the list
         path_queue.add(crt);
         steps_map.put(crt, new Step(crt, 0, manhattan_distance(crt, goal)));
 
         while (!path_queue.isEmpty()) {
 
-            //get current position
-            crt = (MyPoint) path_queue.removeFirst();
-            System.out.println(crt);
-            System.out.println("        " + steps_map.get(crt).getFrom());
+            crt = path_queue.removeFirst();
 
             int g = steps_map.get(crt).getG();
 
-            // to be sure if it reach the goal
             if (isGoalReached(crt)) {
-//                path.add(crt);
-
                 break;
             }
 
-            //to remember all visited
-            //path.add(crt);
             markVisited(crt);
 
-            //put its neighbors in the queue
-            next = crt.moveUp();    //move up
+            next = crt.moveUp();
             if (isInMaze(next) && isClearUp(next) && !isVisited(next)) {
                 path_queue.add(next);
-                steps_map.put(next, new Step(crt, g+1, manhattan_distance(next, goal)));
+                Step toNext = steps_map.get(next);
+
+                if(toNext == null || (toNext != null && toNext.getG()>=g+1)) {
+                    steps_map.put(next, new Step(crt, g + 1, manhattan_distance(next, goal)));
+                }
             }
-            next = crt.moveRight();    //move right
+            next = crt.moveRight();
             if (isInMaze(next) && isClearRight(next) && !isVisited(next)) {
                 path_queue.add(next);
-                steps_map.put(next, new Step(crt, g+1, manhattan_distance(next, goal)));
+                Step toNext = steps_map.get(next);
+
+                if(toNext == null || (toNext != null && toNext.getG()>=g+1)) {
+                    steps_map.put(next, new Step(crt, g + 1, manhattan_distance(next, goal)));
+                }
             }
-            next = crt.moveDown();   //move down
+            next = crt.moveDown();
             if (isInMaze(next) && isClearDown(next) && !isVisited(next)) {
                 path_queue.add(next);
-                steps_map.put(next, new Step(crt, g+1, manhattan_distance(next, goal)));
+                Step toNext = steps_map.get(next);
+
+                if(toNext == null || (toNext != null && toNext.getG()>=g+1)) {
+                    steps_map.put(next, new Step(crt, g + 1, manhattan_distance(next, goal)));
+                }
             }
-            next = crt.moveLeft();    //move left
+            next = crt.moveLeft();
             if (isInMaze(next) && isClearLeft(next) && !isVisited(next)) {
                 path_queue.add(next);
-                steps_map.put(next, new Step(crt, g+1, manhattan_distance(next, goal)));
+                Step toNext = steps_map.get(next);
+
+                if(toNext == null || (toNext != null && toNext.getG()>=g+1)) {
+                    steps_map.put(next, new Step(crt, g + 1, manhattan_distance(next, goal)));
+                }
             }
         }
-
-        System.out.println("===========================================================");
 
         crt = goal;
         path.add(crt);
@@ -95,7 +97,6 @@ public class PathSearcherAStar extends PathSearcher {
         do{
             next = steps_map.get(crt).getFrom();
             path.add(0, next);
-            System.out.println(next);
             crt = next;
         }while(!next.equals(new MyPoint(0, 0, MyPoint.Direction.RIGHT)));
 
